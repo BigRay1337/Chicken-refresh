0230fd236e34d1b823b065292ca4172388e3d506
BigRay1337 <biggamerray@gmail.com>
2026-09-25 19:44:51 -0500

    Make cbDateNowChecked toggle immediately

--- dateNowRefresh.js
+++ dateNowRefresh.js
@@ -1,5 +1,5 @@
 (function () {
-  const REENABLE_DELAY_MS = 1000;
+  const REENABLE_DELAY_MS = 0;
   const SWIPE_THRESHOLD_PX = 60;
   const SWIPE_PENDING_KEY = "chickenDateNowSwipeRefreshPending";
 
